b21c22969064548ff3366f31416a4f472a9b88b0
Raj Sheth <raj@saveo.in>
2019-11-17 01:36:01 +0530

    add login service

--- src/main/java/com/api/gateway/zuul/Gateway.java
+++ src/main/java/com/api/gateway/zuul/Gateway.java
@@ -9,11 +9,9 @@ package com.api.gateway.zuul;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -43,20 +41,51 @@ public class Gateway {
 		this.restTemplate = getNewRestTemplate();
 	}
 
-	@GetMapping("/login")
-	public String login(@RequestBody Object inputObject){
-		Object object = this.restTemplate.getForObject(URI+LOGIN_SERVICE_PORT,Object.class);
-		return "Done";
-	}
-
 	@GetMapping("/search")
 	public Object searchMedicines(@RequestParam String query){
 
-		String url = "http://"+URI+":"+SEARCH_SERVICE_PORT+"search?medicine="+query;
+		String url = "http://" + URI + ":" + SEARCH_SERVICE_PORT + "search?medicine=" + query;
 		System.out.println(url);
 		return restTemplate.getForObject(url,Object.class);
 
 	}
 
-	
+	@PostMapping("/addUser")
+	public ResponseEntity<String> addNewUser(@RequestBody Object userJSON){
+
+		String url = "http://" + URI + ":" + LOGIN_SERVICE_PORT + "/addUser";
+		ResponseEntity<String> message = restTemplate.postForEntity(url, userJSON, String.class);
+		return message;
+	}
+
+
+	@GetMapping("/validate")
+	public String validateUser(@RequestParam String email, @RequestParam String password){
+
+		String url = "http://" + URI + ":" + LOGIN_SERVICE_PORT + "/validate?email="
+				+ email + "&password=" + password;
+		String message = restTemplate.getForObject(url,String.class);
+		return message;
+
+	}
+
+
+	@PostMapping("/addToCart")
+	public ResponseEntity<String> addToCart(@RequestBody Object cartRequest){
+
+		String url = "http://" + URI + ":" + CART_SERVICE_PORT + "/addToCart";
+		return restTemplate.postForEntity(url, cartRequest, String.class);
+
+	}
+
+
+	@PostMapping("/placeOrder")
+	public String addToCart(@RequestParam String userId){
+
+		String url = "http://" + URI + ":" + CART_SERVICE_PORT + "/placeOrder?userId=" + userId;
+		return restTemplate.getForObject(url, String.class);
+
+	}
+
+
 }
